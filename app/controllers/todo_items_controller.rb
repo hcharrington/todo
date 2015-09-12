312d9d52b1c101c22e475b2aaad185742cdc9112
“Hunter <hcharrington@gmail.com>
2015-09-12 09:13:05 -0400

    updated colors -- error with local server

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,22 @@
+class TodoItemsController < ApplicationController
+
+	before_action :set_todo_list
+	
+	def create
+		@todo_item = @todo_list.todo_items.create(todo_items_params)
+
+
+		redirect_to @todo_list
+ 	end
+
+private
+
+def set_todo_list
+		@todo_list = TodoList.find(params[:todo_list_id])
+end
+
+def todo_items_params
+			@todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
+end
+
+end
